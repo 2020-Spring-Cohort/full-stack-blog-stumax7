178650565df1727b04d704f52b52b558fdbd0041
stumax7 <stumax7@gmail.com>
2020-02-29 13:54:52 -0500

    Added post mapping for hashtag to PostController

--- src/main/java/org/wcci/blog/controllers/PostController.java
+++ src/main/java/org/wcci/blog/controllers/PostController.java
@@ -3,16 +3,24 @@ package org.wcci.blog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.wcci.blog.models.HashTag;
 import org.wcci.blog.models.Post;
 import org.wcci.blog.storage.PostStorage;
+import org.wcci.blog.storage.repositories.HashTagRepository;
+
+import java.util.Optional;
 
 @Controller
 public class PostController {
     private PostStorage postStorage;
+    private HashTagRepository hashTagRepository;
 
-    public PostController(PostStorage postStorage){
+    public PostController(PostStorage postStorage, HashTagRepository hashTagRepository){
         this.postStorage = postStorage;
+        this.hashTagRepository = hashTagRepository;
     }
 
     @RequestMapping("/posts/{id}")
@@ -20,5 +28,21 @@ public class PostController {
         Post retrievedPost = postStorage.findPostById(id);
         model.addAttribute("post", retrievedPost);
         return "postView";
+    }
+
+    @PostMapping("/post/{id}/add-hashtag")
+    public String addHashTagToPost(@RequestParam String hashTagName, @PathVariable Long id){
+        HashTag hashTagToAddToPost;
+        Optional<HashTag> hashTagToAddOpt = hashTagRepository.findByName(hashTagName);
+        if(hashTagToAddOpt.isEmpty()){
+            hashTagToAddToPost = new HashTag(hashTagName);
+            hashTagRepository.save(hashTagToAddToPost);
+        } else {
+            hashTagToAddToPost = hashTagToAddOpt.get();
+        }
+        Post postToAddHashTagTo = postStorage.findPostById(id);
+        postToAddHashTagTo.addHashTag(hashTagToAddToPost);
+        postStorage.store(postToAddHashTagTo);
+        return "redirect:/posts/" + id;
     }
 }
--- src/main/java/org/wcci/blog/models/Post.java
+++ src/main/java/org/wcci/blog/models/Post.java
@@ -91,5 +91,8 @@ public class Post {
                 ", postAuthor=" + postAuthor +
                 '}';
     }
+    public void addHashTag(HashTag hashTagToAdd){
+        hashTags.add(hashTagToAdd);
+    }
 }
 
